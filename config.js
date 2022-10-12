cef241a5f18e25bb89d4a66eb5c437fd55519e0b
codejiangqihan <107793872+codejiangqihan@users.noreply.github.com>
2022-10-12 18:46:46 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '各大站点运行状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792830237-8168c6261f608aa0e84991d8',
+    'm792831989-477622087eb587aba07b152c',
+    'm792831996-653f971123e776564ccf09f1',
+    'm792832003-f2f12204c504de5f4742712f',
+    'm792832008-b855c182d3fe884baa0fdf32',
+    
+    
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '启涵的博客',
+      url: 'https://www.codeqihan.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com'
+    },
+  ],
+};
